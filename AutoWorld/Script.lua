a9654f1185493581bcae45980aee5faad6b747df
Llosted <166180836+Llosted@users.noreply.github.com>
2024-04-06 01:51:37 +0300

    Add files via upload

---
+++ AutoWorld/Script.lua
@@ -0,0 +1,10 @@
+getgenv().autoWorldConfig = {
+    AUTO_REBIRTH = false,
+    ZONE_TO_REACH = 149,
+    PURCHASE_CHECK_DELAY = 1
+}
+-- Max Zone For World 1: 99
+-- Max Zone For World 2: 124
+
+local token = "GHSAT0AAAAAACNYYN6ICRCTMNCYVO6WXKZOZQQNO6A"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Qwers1221/Roblox_Scripts/master/PetSimulator99/AutoWorld/WITH_TNT/AutoWorld.lua?token=" .. token))()
