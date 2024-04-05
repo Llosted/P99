6f37f90c1d10aabbdf2adbd7eca32bba4ae71e83
Llosted <166180836+Llosted@users.noreply.github.com>
2024-04-06 01:52:29 +0300

    Update Script.lua

--- AutoWorld/Script.lua
+++ AutoWorld/Script.lua
@@ -6,5 +6,4 @@ getgenv().autoWorldConfig = {
 -- Max Zone For World 1: 99
 -- Max Zone For World 2: 124
 
-local token = "GHSAT0AAAAAACNYYN6ICRCTMNCYVO6WXKZOZQQNO6A"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Qwers1221/Roblox_Scripts/master/PetSimulator99/AutoWorld/WITH_TNT/AutoWorld.lua?token=" .. token))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Qwers1221/Roblox_Scripts/master/PetSimulator99/AutoWorld/WITH_TNT/AutoWorld.lua"))()
